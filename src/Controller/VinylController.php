b7f408a1d8644b9a842d8aaf457b6ac994c70045
Yasser Bal <yasserbal@icloud.com>
2022-09-08 10:40:05 +0200

    Added tutorial files, installed symfony/asset

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -15,26 +15,6 @@ class VinylController extends AbstractController
         $languages = ['PHP', 'JavaScript', 'Swift', 'HTML', '(S)CSS/SASS', 'TypeScript', 'Bash', 'SQL'];
         $technologies = ['ReactJS', 'Angular', 'SwiftUI', 'NodeJS', 'ExpressJS', 'MariaDB', 'TailwindCSS'];
 
-        return $this->render('vinyl/homepage.html.twig', ['name' => 'Yasser', 'tech' => 'Symfony 6', 'languages' => $languages, 'technologies' => $technologies]);
-    }
-    //Instead of using this extra route the parameter in the category route is optional now
-//    #[Route('/browse')]
-//    public function browse(): Response
-//    {
-//        return new Response('Nothing to browse trough...... yet!');
-//    }
-
-    #[Route('/browse/{category}')]
-    public function browseByCategory(string $category = null): Response
-    {
-        //Replacing any dashes with spaces
-        //str_replace('-', ' ', $category);
-        if ($category) {
-            //u is a symfony library that helps with string transformations - in this case we uppercase any letters like in a title
-            $title = u(str_replace('-', ' ', $category))->title(true);
-            return new Response('Nothing to browse in the category: ' . $title
-            );
-        }
-        return new Response('Nothing to browse trough...... yet!');
+        return $this->render('vinyl/homepage.html.twig', []);
     }
 }
