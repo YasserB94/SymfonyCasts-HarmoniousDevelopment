25bb1a0cfadb5a8038ef2adfb3870443644eb37e
Yasser Bal <yasserbal@icloud.com>
2022-09-08 13:36:07 +0200

    Limited Song api route to get requests

---
+++ src/Controller/SongController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/song', name: 'app_song')]
+    public function index(): Response
+    {
+        return $this->render('song/index.html.twig', [
+            'controller_name' => 'SongController',
+        ]);
+    }
+    #[Route('/api/songs/{id}', name: 'app_api_getsong', methods: 'GET')]
+    public function getSong($id): Response
+    {
+        //TODO:: Query to database
+        $song = [
+            'id' => $id,
+            'name' => 'Wonderwall',
+            'url' => "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Oasis/Oasis%20'Wonderwall'.mp3",
+        ];
+        //return $this->json($song) => same 
+        return new JsonResponse($song);
+    }
+}
