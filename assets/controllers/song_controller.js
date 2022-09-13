4ffc76117653464a0c3555bad58eb9ea9cecbaa5
Yasser Bal <yasserbal@icloud.com>
2022-09-13 09:38:46 +0200

    Setup repo for github

--- assets/controllers/song_controller.js
+++ assets/controllers/song_controller.js
@@ -4,12 +4,27 @@ export default class extends Controller {
   static values = {
     songUrl: String,
   };
+  initialize() {
+    this.element.audio = new Audio();
+  }
+
   async play(e) {
     e.preventDefault();
+    //Current song playing ?
+    if (this.element.audio.src === this.songUrlValue) {
+      return;
+    } //Cannot finish got to apply the controller to the parent to keep track if the audio is playing
+    //Anything else playing ?
+    if (!this.element.audio.paused) {
+      console.log("Something was playing");
+      this.element.audio.pause();
+    }
     const res = await axios.get(this.songUrlValue);
+
     if (res) {
-      const audio = new Audio(res.data.url);
-      audio.play();
+      this.element.audio = new Audio(res.data.url);
+      console.log(this.element.audio);
+      this.element.audio.play();
     } else {
       console.error("Someting went wrong");
     }
